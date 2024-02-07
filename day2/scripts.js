98da1ceae97248d1265a3cc23d5c4dd76be627a7
Karina Barros <145608645+KarinaBarros@users.noreply.github.com>
2024-02-07 14:01:37 -0300

    Exercicio Opcional Day 2

--- day2/scripts.js
+++ day2/scripts.js
@@ -3,8 +3,24 @@ function submitButton(){
     var submitIdade = document.getElementById("idade").value;
     var submitLinguagem = document.getElementById("linguagem").value;
     var submitText = "Olá "+ submitNome +", você tem "+submitIdade+" anos e já está aprendendo " +submitLinguagem+ "!";
+    var show = document.getElementById("show");
 
     document.getElementById("text").innerText = submitText;
 
+    show.style.display = 'block';
 
+    document.getElementById("linguagem2").innerText = 'Você gosta de estudar ' + submitLinguagem + '?';
+
+}
+
+function submitButton2(){
+    var submitOption =document.getElementById("option").value;
+    var submitText2 = "Muito bom! Continue estudando e você terá muito sucesso.";
+    var submitText3 = "Ahh que pena... Já tentou aprender outras linguagens?"
+    if (submitOption == 1){
+        document.getElementById("text2").innerText = submitText2;
+    }
+    else{
+        document.getElementById("text2").innerText = submitText3;
+    }
 }
